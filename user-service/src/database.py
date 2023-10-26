9e382e4ca01f954c76fa05c8af84f175548b5c29
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-10-26 15:10:50 +0700

    add video table and requirement.txt

--- user-service/src/database.py
+++ user-service/src/database.py
@@ -22,3 +22,18 @@ class User(db.Model):
     def check_password(self, password):
         return bcrypt.check_password_hash(self._password_hash, password)
 
+class Video(db.Model):
+    __tablename__ = 'videos'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    date_uploaded = db.Column(db.DateTime, default=datetime.utcnow)
+    views = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    s3_filename = db.Column(db.String(255), nullable=False)  # To reference the file on S3
+
+    user = db.relationship('User', backref=db.backref('videos', lazy=True))
+
+    def __repr__(self):
+        return f"<Video(id={self.id}, title={self.title}, user_id={self.user_id})>"
