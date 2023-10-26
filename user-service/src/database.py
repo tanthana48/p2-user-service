948ef51f92ae12c763adc5f548ac085db30ca66c
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-10-26 15:21:35 +0700

    update video table

--- user-service/src/database.py
+++ user-service/src/database.py
@@ -22,18 +22,21 @@ class User(db.Model):
     def check_password(self, password):
         return bcrypt.check_password_hash(self._password_hash, password)
 
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 class Video(db.Model):
-    __tablename__ = 'videos'
-    
+
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    date_uploaded = db.Column(db.DateTime, default=datetime.utcnow)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
     views = db.Column(db.Integer, default=0)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    s3_filename = db.Column(db.String(255), nullable=False)  # To reference the file on S3
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    s3_filename = db.Column(db.String, nullable=False)
+    hls_filename = db.Column(db.String, nullable=True)
+    thumbnail_filename = db.Column(db.String, nullable=True)
 
-    user = db.relationship('User', backref=db.backref('videos', lazy=True))
-
-    def __repr__(self):
-        return f"<Video(id={self.id}, title={self.title}, user_id={self.user_id})>"
+    user = db.relationship("User", backref="videos")
