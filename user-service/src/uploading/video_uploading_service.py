ae539ff448e8f695c921a7fae6c867b0202f037c
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-10-30 20:30:35 +0700

    fix

--- user-service/src/uploading/video_uploading_service.py
+++ user-service/src/uploading/video_uploading_service.py
@@ -3,7 +3,9 @@ from database import User, Video, db
 from botocore.exceptions import NoCredentialsError
 import boto3,time
 from redis import Redis
-from rq import Queue
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 video_uploading_service = Blueprint("video_uploading_service", __name__)
 
@@ -15,63 +17,52 @@ AWS_BUCKET_NAME = 'flasks3scalable'
 
 s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-redis_conn = Redis(host='localhost', port=6379, db=0)
-queue = Queue(connection=redis_conn)
+r = Redis(host='localhost', port=6379, decode_responses=True)
 
-@video_uploading_service.route('/api/upload', methods=['POST'])
-def upload_video():
+@video_uploading_service.route('/api/get-presigned-url', methods=['POST'])
+def get_presigned_url():
+    data = request.json
+    file_name = data['fileName']
+    file_type = data['fileType']
+
+    logging.info(f"Generated presigned URL with ContentType: {file_type}")
+    presigned_url = generate_presigned_url(file_name, file_type)
+    return jsonify({'presigned_url': presigned_url}), 200
+
+@video_uploading_service.route('/api/confirm-upload', methods=['POST'])
+def confirm_upload():
     try:
-        video_file = request.files['video']
-        video_title = request.form['title']
-        video_description = request.form['description']
-        if video_file:
-            if not is_video_too_long(video_file):
-                username = request.form['username']
-                user = User.query.filter_by(username=username).first()
-                if user:
-                    file_name = f"{username}_{int(time.time())}.mp4"
-                    try:
-                        s3_client.upload_fileobj(video_file, AWS_BUCKET_NAME, file_name)
-                        new_video = Video(
-                            title=video_title,
+        data = request.json
+        video_name = data['s3_filename']
+        video_title = data['title']
+        video_description = data['description']
+        username = data['username']
+        try:
+            user = User.query.filter_by(username=username).first()
+            new_video = Video(title=video_title, 
                             description=video_description,
                             user_id=user.id,
-                            s3_filename=file_name
-                        )
-                        db.session.add(new_video)
-                        db.session.commit()
-                        video = Video.query.filter_by(s3_filename=file_name).first()
-                        presigned_url = generate_presigned_url(video_file.filename)
-                        video_details = {
-                            'id': video.id,
-                            's3_filename': file_name
-                        }
-                        job = queue.enqueue('process_video_task', video_details)
-
-                        return jsonify({'presigned_url': presigned_url}), 200
-                    except NoCredentialsError:
-                        return jsonify({'error': 'AWS credentials not available'}), 403
-                else:
-                    return jsonify({'error': 'User not found'}), 404
-            else:
-                return jsonify({'error': 'Video is too long (max: 1 minute)'}), 400
-        else:
-            return jsonify({'error': 'Video file missing'}), 400
+                            s3_filename=video_name)
+            db.session.add(new_video)
+            db.session.commit()
+            return jsonify({'message': 'Video uploaded successfully'}), 200
+        except NoCredentialsError:
+            return jsonify({'error': 'AWS credentials not available'}), 403
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-def is_video_too_long(video_file):
-    max_duration = 60 
-    if video_file.content_length > max_duration: return True 
-    return False
-
-def generate_presigned_url(filename):
+def generate_presigned_url(filename, file_type):
     presigned_url = s3_client.generate_presigned_url(
         'put_object',
-        Params={'Bucket': AWS_BUCKET_NAME, 'Key': filename},
-        ExpiresIn=3600  
+        Params={
+            'Bucket': AWS_BUCKET_NAME,
+            'Key': filename,
+            'ContentType': file_type
+        },
+        ExpiresIn=3600
     )
     return presigned_url
+
 
 @video_uploading_service.route('/api/videos', methods=['GET'])
 def get_videos():
